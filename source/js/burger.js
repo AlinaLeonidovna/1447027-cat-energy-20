e06c1eb660d89b90976ab03544114a63e7ce67d0
AlinaLeonidovna <anisimova.alina.leonidovna@gmail.ru>
2020-09-01 23:51:19 +0300

    Добавляет бордер шапке, градиент слайдеру

--- source/js/burger.js
+++ source/js/burger.js
@@ -1,7 +1,3 @@
-// document.querySelector(".page-header__toggle").onclick = function(){
-//   document.querySelector(".page-header__toggle").classList.toggle("page-header__toggle--active");
-// }
-
 var burgerButton = document.querySelector(".page-header__toggle")
 var mainNav = document.querySelector(".main-nav")
 

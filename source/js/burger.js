91231682bdad4857f77c40527b28869245cd8bcb
AlinaLeonidovna <anisimova.alina.leonidovna@gmail.ru>
2020-09-03 15:37:59 +0300

    Добавляет правки по меню на JS, правки по критериям

--- source/js/burger.js
+++ source/js/burger.js
@@ -1,7 +1,13 @@
-var burgerButton = document.querySelector(".page-header__toggle")
-var mainNav = document.querySelector(".main-nav")
+var burgerButton = document.querySelector(".page-header__toggle");
+var mainNav = document.querySelector(".main-nav");
 
-  burgerButton.addEventListener("click", function () {
+  burgerButton.classList.add("page-header__toggle--close");
+  mainNav.classList.remove("main-nav--open");
+  mainNav.classList.add("main-nav--close");
+
+burgerButton.addEventListener("click", function() {
+  burgerButton.classList.toggle("page-header__toggle--close");
+  burgerButton.classList.toggle("page-header__toggle--open");
+  mainNav.classList.toggle("main-nav--close");
   mainNav.classList.toggle("main-nav--open");
-  burgerButton.classList.toggle("page-header__toggle--active")
-})
+});
